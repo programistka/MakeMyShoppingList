af169d30130870b94dbe4efc70367bb478021ce7
programistka <iwona.lalik@gmail.com>
2016-04-29 06:46:15 +0200

    Refactoring

--- app/src/main/java/net/programistka/shoppingadvisor/PredictionsHandler.java
+++ app/src/main/java/net/programistka/shoppingadvisor/PredictionsHandler.java
@@ -8,29 +8,37 @@ import java.util.ArrayList;
  * Created by maga on 28.03.16.
  */
 public class PredictionsHandler {
+    private static final int FACTOR = 1000*3600*24;
+
     public static Prediction getPrediction(ArrayList<Long> shoppingTimes) {
         long current = shoppingTimes.get(0);
-        System.out.println("get0" + shoppingTimes.get(0));
         long next = shoppingTimes.get(1);
-        System.out.println("get1" + shoppingTimes.get(1));
-        long predictionTime = next - current;
-        System.out.println(predictionTime);
-        Prediction prediction = new Prediction();
         if(shoppingTimes.size() == 2) {
-            prediction.setDays_number((int)(next - current)/ (1000*3600*24));
-            prediction.setTime(next + next - current);
+            return generatePredictionForTwoItems(current, next);
         } else {
-            for (int i = 2; i < shoppingTimes.size(); i++) {
-                current = next;
-                next = shoppingTimes.get(i);
-                predictionTime += next - current;
-                System.out.println("item i=" + i + shoppingTimes.get(i));
-            }
-            double time = predictionTime / (shoppingTimes.size() - 1);
-            int days = (int) Math.round(time/(1000*3600*24));
-            prediction.setTime(next + days*3600*24*1000);
-            prediction.setDays_number(days);
+            return generatePredictionForMoreThanTwoItems(shoppingTimes, current, next);
         }
+    }
+
+    private static Prediction generatePredictionForMoreThanTwoItems(ArrayList<Long> shoppingTimes, long current, long next) {
+        long predictionTime = next - current;
+        for (int i = 2; i < shoppingTimes.size(); i++) {
+            current = next;
+            next = shoppingTimes.get(i);
+            predictionTime += next - current;
+        }
+        double time = predictionTime / (shoppingTimes.size() - 1);
+        int days = (int) Math.round(time/(factor));
+        Prediction prediction = new Prediction();
+        prediction.setTime(next + days*factor);
+        prediction.setDays_number(days);
+        return prediction;
+    }
+
+    private static Prediction generatePredictionForTwoItems(long current, long next) {
+        Prediction prediction = new Prediction();
+        prediction.setDays_number((int)(next - current)/factor);
+        prediction.setTime(next + next - current);
         return prediction;
     }
 }
