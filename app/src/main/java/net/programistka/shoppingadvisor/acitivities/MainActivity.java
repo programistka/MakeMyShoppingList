784195cd3d23a8d164c82da4da20ad707e303b6b
programistka <iwona.lalik@gmail.com>
2016-04-29 06:46:15 +0200

    Butter knife on main screen

--- app/src/main/java/net/programistka/shoppingadvisor/acitivities/MainActivity.java
+++ app/src/main/java/net/programistka/shoppingadvisor/acitivities/MainActivity.java
@@ -6,15 +6,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import net.programistka.shoppingadvisor.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends ActivityWithFab {
+
+    @BindView(R.id.addShoppingItem) Button addShoppingItem;
+    @BindView(R.id.showHistory) Button showHistory;
+    @BindView(R.id.showPredictions) Button showPredictions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         attachFabAction();
@@ -32,14 +43,17 @@ public class MainActivity extends ActivityWithFab {
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
-    public void loadShowEmptyItemsHistoryView(View view){
-        startActivity(new Intent(this, ShowEmptyItemsHistoryActivity.class));
-    }
-
+    @OnClick(R.id.addShoppingItem)
     public void loadAddEmptyItemView(View view){
         startActivity(new Intent(this, AddEmptyItemActivity.class));
     }
 
+    @OnClick(R.id.showHistory)
+    public void loadShowEmptyItemsHistoryView(View view){
+        startActivity(new Intent(this, ShowEmptyItemsHistoryActivity.class));
+    }
+
+    @OnClick(R.id.showPredictions)
     public void loadShowPredictionsView(View view){
         startActivity(new Intent(this, ShowPredictionsActivity.class));
     }
